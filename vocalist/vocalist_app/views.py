e887fcf0d1065fc67399d31749a574e64cf16cf2
MNeipp <ReamusLQ@gmail.com>
2020-07-17 11:54:37 -0600

    removed AJAX-posting reviews,fixed star rating

--- vocalist/vocalist_app/views.py
+++ vocalist/vocalist_app/views.py
@@ -42,35 +42,23 @@ def company_profile(request, slug):
         }
     return render(request, "company_profile.html", context)
 
-def leave_review(request, slug):
-    if request.method == "GET":
-        return redirect(reverse("index"))
-    else:
-        company = Company.objects.get(slug=slug)
-        logged_user = User.objects.get(id=request.session['user_id'])
-        if "anonymous" in request.POST:
-            new_review = Review.objects.create(content = request.POST['content'], rating = request.POST['rating'], creator = logged_user, company = company)
-        else:
-            new_review = Review.objects.create(content = request.POST['content'], rating = request.POST['rating'], creator = logged_user, company = company, anonymous=False)
-        return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
-
-def ajax_review(request, slug):
+def create_review(request, slug):
     company = Company.objects.get(slug=slug)
     logged_user = User.objects.get(id=request.session['user_id'])
     if "anonymous" in request.POST:
         new_review = Review.objects.create(content = request.POST['content'], rating = request.POST['rating'], creator = logged_user, company = company)
     else:
         new_review = Review.objects.create(content = request.POST['content'], rating = request.POST['rating'], creator = logged_user, company = company, anonymous=False)
-    context ={
-        "review":new_review
-    }
-    return render(request, "snippets/review_snippet.html", context)
+    return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
 
 def delete_review(request, review_id):
     if request.method == "POST":
         to_delete = Review.objects.get(id=review_id)
         to_delete.delete()
-    return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
+    context={
+        "logged_user":User.objects.get(id=request.session['user_id'])
+    }
+    return render (request, "snippets/user_reviews_snippet.html", context)
 
 def edit_review(request, review_id):
     pass
