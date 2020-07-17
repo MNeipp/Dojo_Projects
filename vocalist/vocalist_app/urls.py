6392cd7fd7ff73a55958fb911ddeb8c2f2f34e0b
MNeipp <ReamusLQ@gmail.com>
2020-07-16 18:04:21 -0600

    post reviews with AJAX

--- vocalist/vocalist_app/urls.py
+++ vocalist/vocalist_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns=[
     path('the-list/', views.the_list, name="list"),
     path('the-list/<slug:slug>', views.company_profile, name="company_profile"),
     path('the-list/<slug:slug>/review', views.leave_review, name="leave_review"),
+    path('the-list/<slug:slug>/review/ajax', views.ajax_review, name="ajax_review"),
+    path('the-list/delete_review/<int:review_id>', views.delete_review, name="delete_review"),
+    path('the-list/edit_review/<int:review_id>', views.edit_review, name="edit_review")
 ]
--- vocalist/vocalist_app/views.py
+++ vocalist/vocalist_app/views.py
@@ -19,21 +19,28 @@ def index(request):
 
 def the_list(request):
     company_list = Company.objects.all()
-    context={
-        'companies': CompanyFilter(request.GET, queryset=company_list),
-        'logged_user': User.objects.get(id=request.session['user_id'])
-    }
+    if 'user_id' in request.session:
+        context={
+            'companies': CompanyFilter(request.GET, queryset=company_list),
+            'logged_user': User.objects.get(id=request.session['user_id'])
+        }
+    else:
+        context={
+            'companies': CompanyFilter(request.GET, queryset=company_list),
+        }
     return render(request, "list.html", context)
 
 def company_profile(request, slug):
     if 'user_id' not in request.session:
-        return render(request, "company_profile.html")
+        context={
+            'company': Company.objects.get(slug=slug),
+        }
     else:
         context={
             'company': Company.objects.get(slug=slug),
             'logged_user': User.objects.get(id=request.session['user_id'])
         }
-        return render(request, "company_profile.html", context)
+    return render(request, "company_profile.html", context)
 
 def leave_review(request, slug):
     if request.method == "GET":
@@ -46,6 +53,27 @@ def leave_review(request, slug):
         else:
             new_review = Review.objects.create(content = request.POST['content'], rating = request.POST['rating'], creator = logged_user, company = company, anonymous=False)
         return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
+
+def ajax_review(request, slug):
+    company = Company.objects.get(slug=slug)
+    logged_user = User.objects.get(id=request.session['user_id'])
+    if "anonymous" in request.POST:
+        new_review = Review.objects.create(content = request.POST['content'], rating = request.POST['rating'], creator = logged_user, company = company)
+    else:
+        new_review = Review.objects.create(content = request.POST['content'], rating = request.POST['rating'], creator = logged_user, company = company, anonymous=False)
+    context ={
+        "review":new_review
+    }
+    return render(request, "snippets/review_snippet.html", context)
+
+def delete_review(request, review_id):
+    if request.method == "POST":
+        to_delete = Review.objects.get(id=review_id)
+        to_delete.delete()
+    return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
+
+def edit_review(request, review_id):
+    pass
 
  
 
